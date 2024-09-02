995c4d6c63b26e8ca8b28fd34f9f25852d441d3a
Kira <kira.eva@protonmail.com>
2024-09-02 22:27:23 +0100

    selected glow

--- code/Pawn.cs
+++ code/Pawn.cs
@@ -9,19 +9,19 @@ public class Pawn : Component, ISelectable, IUnit
     [Property] public string Name { get; set; }
     [Property] public int Level { get; set; }
 
-    [Property] private SkinnedModelRenderer Renderer { get; set; }
-    [Property] private GameObject OutlineObject { get; set; }
+    [Property, Category("Outline")] private SkinnedModelRenderer Renderer { get; set; }
+    [Property, Category("Outline")] private GameObject OutlineObject { get; set; }
+    [Property, Category("Outline")] private bool UseOutlineModel { get; set; }
+    [Property] private GameObject SelectionGlow { get; set; }
 
     [Property] private Color HoveringTint { get; set; } = Color.Cyan.WithRed(220);
     [Property] private Color SelectedTint { get; set; } = Color.Cyan;
-
-    public bool IsSelected { get; set; }
-    public bool IsHovering { get; set; }
-
-    public SelectableTypes SelectableType { get; set; } = SelectableTypes.Unit;
     private Color Tint { get; set; }
 
+    public SelectableTypes SelectableType { get; set; } = SelectableTypes.Unit;
     public Stats Stats { get; set; } = new Stats();
+    public bool IsSelected { get; set; }
+    public bool IsHovering { get; set; }
 
     protected override void OnAwake()
     {
@@ -32,32 +32,35 @@ public class Pawn : Component, ISelectable, IUnit
     public void OnHover()
     {
         IsHovering = true;
+        Renderer.Tint = HoveringTint;
 
         if (!IsSelected)
         {
-            OutlineObject.Enabled = true;
-            Renderer.Tint = HoveringTint;
+            if (UseOutlineModel) OutlineObject.Enabled = true;
         }
     }
 
     public void OnLeaveHover()
     {
         IsHovering = false;
-        OutlineObject.Enabled = IsSelected;
         Renderer.Tint = !IsSelected ? Tint : SelectedTint;
+        // SelectionGlow.Enabled = IsSelected;
+        // if (UseOutlineModel) OutlineObject.Enabled = IsSelected;
     }
 
     public void Select()
     {
         IsSelected = true;
-        OutlineObject.Enabled = true;
-        Renderer.Tint = SelectedTint;
+        if (UseOutlineModel) OutlineObject.Enabled = true;
+        SelectionGlow.Enabled = true;
+        // Renderer.Tint = SelectedTint;
     }
 
     public void Deselect()
     {
         IsSelected = false;
-        OutlineObject.Enabled = false;
+        if (UseOutlineModel) OutlineObject.Enabled = false;
+        SelectionGlow.Enabled = false;
         Renderer.Tint = Tint;
     }
 }
